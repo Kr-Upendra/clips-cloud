74f12da3720fc86ebfe6aae76e884b60dbd8518e
Upendra Kumar <toxic74412@gmail.com>
2024-09-21 16:24:01 +0530

    change image and content

--- src/containers/Platform.tsx
+++ src/containers/Platform.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
+import youtube from "../../public/platform/youtube.svg";
 import instagram from "../../public/platform/instagram.svg";
+import facebook from "../../public/platform/facebook.svg";
+import x from "../../public/platform/x.svg";
+
+const images = [youtube, instagram, facebook, x];
+const platforms = ["Youtube", "Instagram", "Facebook", "X (Twitter)"];
 
 export default function Platform() {
   return (
@@ -25,7 +31,7 @@ export default function Platform() {
             >
               <div className="text-center">
                 <Image
-                  src={instagram}
+                  src={images[index]}
                   className="mx-auto select-none"
                   alt="instagram logo"
                   width={170}
@@ -33,7 +39,9 @@ export default function Platform() {
                 />
               </div>
               <div className="text-center">
-                <h1 className="text-xl text-white font-bold mb-3">Instagram</h1>
+                <h1 className="text-xl text-white font-bold mb-3">
+                  {platforms[index]}
+                </h1>
               </div>
               {/* <div className="absolute bottom-0 left-0 rounded-b-lg px-2 z-10 py-3 bg-gradient-to-b from-[#00000027] to-[#5900ff79] ">
                 <p className="text-xs text-gray-500">
